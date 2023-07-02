00672b632fba53aaf53b4754d367197e223a0ac1
nandhas20ITR069 <88595743+nandhas20ITR069@users.noreply.github.com>
2023-07-02 18:55:01 +0530

    new url

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ import store from './store';
 import { useEffect } from 'react';
 import axios from 'axios';
 
-axios.defaults.baseURL = ""
+axios.defaults.baseURL = "appsamudhrikalakshana.onrender.com";
 function App() {
 
   useEffect(()=>{      
